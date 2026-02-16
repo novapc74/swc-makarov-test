4b94ecbcf0518a19fc1a1ae824e696b1d2aa028a
novapc74 <novapc74@yandex.ru>
2026-02-16 11:37:03 +0300

    add header for cache as HIT or MISS

--- app/Http/Middleware/CacheResponse.php
+++ app/Http/Middleware/CacheResponse.php
@@ -22,10 +22,16 @@ class CacheResponse
             $key = "user_{$userId}_" . md5($url);
 
             return Cache::tags(["user_{$userId}_tasks"])->remember($key, $ttl, function () use ($next, $request) {
-                return $next($request);
+                $response = $next($request);
+                $response->headers->set('X-Cache', 'HIT');
+
+                return $response;
             });
         }
 
-        return $next($request);
+        $response = $next($request);
+        $response->headers->set('X-Cache', 'MISS');
+
+        return $response;
     }
 }
